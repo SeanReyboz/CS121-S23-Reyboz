0a6143d76ec81bebaf0b1e2eb9f1f5c8969fef61
Sean Reyboz <71266774+SeanReyboz@users.noreply.github.com>
2023-01-17 10:43:19 -0500

    Initial commit

---
+++ Practice/src/WeekOne/Reyboz.java
@@ -0,0 +1,23 @@
+package WeekOne;
+
+public class Reyboz
+{
+    public static void main(String[] args) {
+        String firstname;
+        int age;
+        char letterGrade;
+        double gpa;
+        boolean csMajor;
+        String favColor;
+
+        firstname = "Sean";
+        age = 20;
+        letterGrade = 'A';
+        gpa = 3.815;
+        csMajor = true;
+        favColor = "light-pink";
+
+        System.out.println(firstname + "'s gpa is " + gpa);
+
+    }
+}
