804d4c4b94263f2a106678cb5fa187e07542890b
Sean Reyboz <71266774+SeanReyboz@users.noreply.github.com>
2023-01-24 10:03:00 -0500

    Created Project 1 boilerplate

---
+++ Project1/src/PokemonBattle.java
@@ -0,0 +1,17 @@
+/** *************************************************************************
+ *                      Project 1 - Pokemon Battle
+ *
+ *  Name: Sean REYBOZ
+ *  Date: 01/23/23
+ *  Last modified: 01/23/23
+ *
+ *  Summary:
+ *  This class
+ */
+
+public class PokemonBattle
+{
+    public static void main(String[] args) {
+
+    }
+}
